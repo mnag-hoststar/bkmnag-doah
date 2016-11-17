e94f91510080c4f9139972c960330f07ee8976e1
Gabriela Schnell <gsc@hoststar.ch>
2016-11-17 10:07:09 +0100

    changes js and layout

--- js/floating-label.js
+++ js/floating-label.js
@@ -1,3 +1,14 @@
 $('input').on('focus blur', function (e) {
     $(this).parents('.form-group').toggleClass('focused', (e.type === 'focus' || this.value.length > 0));
 }).trigger('blur');
+
+$(document).on('focus active visited', 'select', function () {
+    $('.label--filter, .label--variation').addClass('active');
+    $('select').addClass('selected');
+});
+$(document).on('blur', 'select', function (e) {
+
+    if (!$(e.currentTarget).val())
+        $('label').removeClass('active');
+
+});
