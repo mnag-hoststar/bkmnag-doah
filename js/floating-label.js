5608dd690cd9742157e36d1e59d3d99c0211a816
Gabriela Schnell <gsc@hoststar.ch>
2016-11-22 11:04:34 +0100

    floating label test

--- js/floating-label.js
+++ js/floating-label.js
@@ -1,4 +1,4 @@
-$('input, textarea').on('focus blur', function (e) {
+/*$('input, textarea').on('focus blur', function (e) {
     $(this).parents('.form-group').toggleClass('focused', (e.type === 'focus' || this.value.length > 0));
 }).trigger('blur');
 
