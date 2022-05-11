d8b2141d2b86872ddcce81943f70a9d65c25abb9
Eva Jutzeler <eju@hoststar.ch>
2022-05-11 18:10:06 +0200

    #450 Doah: Fix template bugs for migration

--- js/navigation.js
+++ js/navigation.js
@@ -41,5 +41,13 @@ $(document).ready(function() {
 
 	$(document).bind("DOMSubtreeModified",function(){
         $('.feature__content-wrap').css('padding-top', (navigationSpace())+'px');
+	});
+	
+	// ------------------------------
+    // Hides other submenus when a main menu point with a submenu is clicked.
+	// ------------------------------
+    
+    $(".navigation-item").click(function(){
+        $(this).siblings(".navigation-item").removeClass("folder-open open");
     });
 });
